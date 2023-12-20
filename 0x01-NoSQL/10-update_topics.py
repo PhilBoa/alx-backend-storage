20e9c7890f3c3b4d4093088fedb0ef640c8667e2
PhiBoa <philicbrefo98@gmail.com>
2023-12-20 05:11:23 +0000

    10-update_topics.py

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+Module to interact with MongoDB collections.
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    Changes all topics of a school document based on the name.
+
+    Args:
+        mongo_collection (pymongo.collection.Collection): The MongoDB
+        collection object.
+        name (str): The school name to update.
+        topics (list of str): The list of topics approached in the school.
+
+    Returns:
+        None
+    """
+    mongo_collection.update_many(
+        {"name": name},
+        {"$set": {"topics": topics}}
+    )
